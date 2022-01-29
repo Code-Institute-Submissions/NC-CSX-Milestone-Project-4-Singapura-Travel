1f4c65955395b41b395756bde20f2cf3c3974557
NC-CSX <niallcrk@hotmail.co.uk>
2022-01-29 21:55:41 +0000

    customised admin packages name

--- packages/admin.py
+++ packages/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 from .models import Packages, Category
 
 # Register your models here.
-admin.site.register(Packages)
-admin.site.register(Category)
+
+class PackageAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+admin.site.register(Packages, PackageAdmin)
+admin.site.register(Category, CategoryAdmin)
