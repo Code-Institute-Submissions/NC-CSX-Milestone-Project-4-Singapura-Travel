f30ef5325f9ad3b76e94fb368453e33834da2040
NC-CSX <niallcrk@hotmail.co.uk>
2022-02-17 22:13:24 +0000

    Added fixtures to database

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Goods, Category
 
 # Register your models here.
+
+class PackageAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+admin.site.register(Goods, PackageAdmin)
+admin.site.register(Category, CategoryAdmin)
